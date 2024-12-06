f60e5a19d8dedbf14e6cbb7639f8d85bc9917f52
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-12-06 19:36:24 +0900

    feat: Integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// Architectural patternt: MVC, Dependency Injection, MVP
+// MVC = Model VIEW CONTROLLER
+// Design pattern: Middle, Decotar
+
+// import moment from 'moment'; // const moment  = require('moment);
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
