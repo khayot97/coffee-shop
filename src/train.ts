c5725ca4a30acde9e91c5ab588ef32c11f0c3f23
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-12-07 21:48:15 +0900

    feat:TASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+// H-TASK: 
+
+// shunday function tuzing, u integerlardan iborat arrayni
+//  argument sifatida qabul qilib, faqat positive qiymatlarni 
+//  olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(numbers: number[]): string {
+    // 1-qadam: Musbat sonlarni tanlab olish!
+    return numbers.filter(num1 => num1 > 0).join('');
+    // 2-qadam filtrlab [1, 2] keyin join('12)
+
+}
+
+const result = getPositive([1, -4, 2]);
+console.log("result:", result);
