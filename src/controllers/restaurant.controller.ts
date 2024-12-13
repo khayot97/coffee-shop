4b305ec2ba3c57dc2b305c2da9c3a368ceb620ad
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-12-13 20:20:29 +0900

    feat: Develop restaurant login process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberSevice from "../models/Member.service"; 
 import { log } from "console";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
@@ -33,10 +33,16 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     }
 };
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
     try {
         console.log("processLogin ");
-        res.send("DONE");
+        console.log("body:", req.body);
+        const input: LoginInput = req.body;
+
+        const memberService = new MemberSevice();
+        const result = await memberService.processLogin(input);
+
+        res.send(result);
     } catch (err) {
         console.log("Error processLogin:", err)
     }
--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -13,6 +13,10 @@ export enum Message {
     NO_DATA_FOUND = "No data id found!",
     CREATE_FAILED = "Create is failed!",
     UPDATE_FAILED = "Updated is failed!",
+    
+    USED_NICK_PHONE = "You are inserting already used nick or phone!",
+    NO_MEMBER_NICK = "No member with that member nick!",
+    WRONG_PASSWORD = "Wrong paasword,  please try again!",
 }
 
 class Errors extends Error {
