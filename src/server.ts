6a898cc1e50fa794c5ecc40a2e63703eed83d974
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-12-17 23:22:32 +0900

    feat: Develop signup process

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,11 @@ import app from "./app"
 
 mongoose
 .connect(process.env.MONGO_URL as string, {})
-.then(data => {
+.then((data) => {
     console.log("MongoDB connection succsed")
     const PORT = process.env.PORT ?? 3003; 
     app.listen(PORT, function () {
         console.log(`The server is running successfully on port: ${PORT}`);
     });
 })
-.catch((err) => console.log("ERROR on connection MongoDB",err));
+.catch((err) => console.log("ERROR on connection MongoDB", err));
