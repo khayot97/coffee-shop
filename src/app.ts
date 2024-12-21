7cbc454fead8d7721c954ab27b40ba19fa43f854
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-12-21 22:03:11 +0900

    fix: Create sessions on login and signup process

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,16 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from './libs/config';
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import ConnectMongoDBSession from 'connect-mongodb-session';
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +23,19 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
+
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),
+        cookie: {
+          maxAge: 1000 * 360 * 3,  // 3h
+        },
+        store: store,
+        resave: true,
+        saveUninitialized: true
+      })
+);
+
 
 /** 3-VIEWS **/
 app.set('views', path.join(__dirname, 'views'));
