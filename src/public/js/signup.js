1020d114360bbd247c488f168ec07a4fbe50896d
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-01-02 21:14:53 +0900

    feat: Signup  process and  frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,30 @@
+const { name } = require("ejs");
+
 console.log("Signup frontend javascript file");
-$(function () {});
+$(function () {
+    const fileTraget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTraget.on("change", function () {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].file[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if (!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg and png!");
+            } else  {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("success");
+                }
+                filename = $(this) [0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+});
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -22,5 +47,13 @@ function validateSignupForm() {
         alert("Password deffers, please check");
         return false;
     }
- 
+
+    const memberImage = $(".member-image").get(0).file[0]
+    ? $(".member-image").get(0).file[0].name
+    : null;
+    if (!memberImage) {
+        alert("Please insert restaurant image!");
+        return false;
+    }
+
 }
