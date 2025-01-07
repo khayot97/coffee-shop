735a119b3afc3765456846ad6cb5524c0ba1f2cf
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-01-07 20:03:00 +0900

    fix: Users page render real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -119,8 +119,6 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
     try {
         console.log("getUsers");
         const result = await memberService.getUsers();
-        //test uchun
-        // console.log("result:", result);
         res.render("users", { users: result });
     } catch (err) {
         console.log("Error, getUsers:", err);
