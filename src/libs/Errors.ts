a7a1a257f8bd9d9d895e42600945ddece7e8bcf9
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-01-07 20:46:19 +0900

    fix: Modify spa login api process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -13,8 +13,10 @@ export enum Message {
     NO_DATA_FOUND = "No data id found!",
     CREATE_FAILED = "Create is failed!",
     UPDATE_FAILED = "Updated is failed!",
+
     USED_NICK_PHONE = "You are inserting already used nick or phone!",
     NO_MEMBER_NICK = "No member with that member nick!",
+    BLOCEKED_USER = "You have been blocked, contact restaurant!",
     WRONG_PASSWORD = "Wrong paasword,  please try again!",
     NOT_AUTHENTICATED = "You are not authenticated, Please login first!"
 }
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,7 @@
 import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { log } from "console";
 import { shapeIntoMongooseObjectId } from "../libs/config";
@@ -31,16 +31,21 @@ public async signup(input: MemberInput): Promise<Member> {
     }
 }
 
-// Login
+/** LOGIN */
 public async login(input: LoginInput): Promise<Member> {
-    // TODO: Consider member status later 
     const member = await this.memberModel
         .findOne(
-            { memberNick: input.memberNick },
-            { memberNick: 1, memberPassword: 1 }
+            { 
+                memberNick: input.memberNick,
+                memberStatus:  { $ne: MemberStatus.DELETE},
+             },
+            { memberNick: 1, memberPassword: 1, memberStatus: 1 }
         )
         .exec();
     if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+    else if (member.memberStatus === MemberStatus.BLOCK) {
+        throw new Errors(HttpCode.FORBIDDIN, Message.BLOCEKED_USER);
+    }
 
     const isMatch = await bcrypt.compare(
         input.memberPassword,
